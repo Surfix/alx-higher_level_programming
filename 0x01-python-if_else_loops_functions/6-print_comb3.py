e17389a734ba0d2d9402e28ad64130e6c2fc310c
Surfix <adebisi.naheem@gmail.com>
2022-09-22 03:21:36 +0000

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
   for j in range(1,10):
     if i < j:
       if i == 8 and j == 9:
-        print(f"{i}{j}\n")
+        print("{}{}\n".format(i,j))
       else:
-        print(f"{i}{j}, ")
+        print("{i}{j}, ".format(i,j))
