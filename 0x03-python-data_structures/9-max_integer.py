d4c31603f711afb8731f878fba04fb0979d0f8ce
Surfix <adebisi.naheem@gmail.com>
2022-09-26 20:04:26 +0000

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    biggest = my_list[0]
-    
-    for i in range(len(my_list)):
-        if biggest < my_list[i]
-            biggest = my_list[i]
-    return biggest
-    
+    maxnum = my_list[0]
+
+    for num in my_list:
+        if num > maxnum:
+            maxnum = num
+    return maxnum
