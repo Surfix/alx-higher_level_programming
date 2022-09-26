73b2693ff67aabc37df6147025adc2595ff4ff0d
Surfix <adebisi.naheem@gmail.com>
2022-09-26 17:53:02 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    len_ = len(sentence)
-    if len_ == 0:
-        first_char = None
-    else:
-        first_char = sentence[0]
-    return len_, first_char
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
