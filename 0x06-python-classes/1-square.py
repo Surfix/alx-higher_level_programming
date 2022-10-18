23f8900712ffd547d39387100758cb70c3b7dd1c
Surfix <adebisi.naheem@gmail.com>
2022-10-18 21:56:34 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,7 @@
 """Creates a new type of Square Class"""
 
 class Square:
-
+    """Define the Class"""
     def __init__(self, size):
-        """Initialize the squar type"""
+        """Initialize the square size"""
         self.__size = size
