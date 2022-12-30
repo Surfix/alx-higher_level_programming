053379851d60611fcbad154f951a02e0b67627b3
Naheem Adebisi <adebisi.naheem@gmail.com>
2022-12-30 13:31:53 +0000

    1. Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
-class Base():
-   __nb_objects = 0
-   def __init__(self, id=None):
-      if id != None:
-         self.id = id
-      else:
-         __nb_objects +=1
-         self.id = __nb_objects
+class Base:
+    '''the Base class'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''init magic'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
