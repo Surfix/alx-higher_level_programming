bea97cbaeb143f35492ab255ad79c6cd8061a1f7
Naheem Adebisi <adebisi.naheem@gmail.com>
2023-01-01 20:09:26 +0000

    16-19 update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,47 @@ class Base:
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a json of an object to a file."""
+        if not list_objs:
+            with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+                f.write("[]")
+        else:
+            ans = []
+            for obj in list_objs:
+                ans.append(obj.to_dictionary())
+            with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+                f.write(cls.to_json_string(ans))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the object representation of the json object."""
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance of base."""
+        fake = None
+        if cls.__name__ == "Rectangle":
+            fake = cls(2, 2)
+        else:
+            fake = cls(3)
+        fake.update(**dictionary)
+        return fake
+
+    @classmethod
+    def load_from_file(cls):
+        """Convert json file to list of instances."""
+        instances = []
+        try:
+            with open(cls.__name__ + ".json", "r", encoding="utf-8") as f:
+                items = cls.from_json_string(f.read())
+                for its in items:
+                    instances.append(cls.create(**its))
+            return instances
+        except Exception:
+            return []
