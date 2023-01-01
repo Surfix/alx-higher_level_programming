83e5a390b67344115897c8b78891393c1edebc14
Naheem Adebisi <adebisi.naheem@gmail.com>
2023-01-01 23:59:33 +0000

    unittest

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module for Rectangle unit tests.'''
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    '''Tests the Rectangle class'''
+    pass
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''my Square unittests'''
+import unittest
+from models.base import Base
+from models.square import Square
+
+class TestSquare(unittest.TestCase):
+    '''test the circle class.'''
+    pass
